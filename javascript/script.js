00c29640ba3d819c2213b2f7804f9adf3f4cf7f6
Tom <tombradley0023@gmail.com>
2026-06-25 00:50:10 +0100

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,35 +1,26 @@
 window.addEventListener("load", () => {
+  document.body.style.opacity = "1";
 
-    document.body.style.opacity = "1";
+  /* COMMAND TEAM IMAGE SYSTEM */
+  const staffImages = {
+    "T. Walker": "twalker.png",
+    "C. White": "cwhite.png",
+    "J. Johnson": "jjohnson.png",
+    "J. Parrott": "jparrott.png",
+    "M. Wallace": "mwallace.png",
+    "M. Rainer": "mrainer.png",
+    "C. Murray": "cmurray.png"
+  };
 
-    /* COMMAND TEAM IMAGE SYSTEM */
+  const cards = document.querySelectorAll(".card");
 
-    const staffImages = {
+  cards.forEach((card) => {
+    const name = card.dataset.name;
+    const image = card.querySelector(".profile");
 
-        "T. Walker": "twalker.png",
-        "C. White": "cwhite.png",
-        "J. Johnson": "jjohnson.png",
-        "J. Parrott": "jparrott.png",
-        "M. Wallace": "mwallace.png",
-        "M. Rainer": "mrainer.png",
-        "C. Murray": "cmurray.png"
-
-    };
-
-    const cards = document.querySelectorAll(".card");
-
-    cards.forEach(card => {
-
-        const name = card.dataset.name;
-        const image = card.querySelector(".profile");
-
-        if (staffImages[name] && image) {
-
-            image.src = "../assets/team/" + staffImages[name];
-            image.alt = name;
-
-        }
-
-    });
-
+    if (staffImages[name] && image) {
+      image.src = "../assets/team/" + staffImages[name];
+      image.alt = name;
+    }
+  });
 });
